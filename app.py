5eb63267f63d1c20db734b11bb689217dcede576
Eduardo Machado <duduardo096@gmail.com>
2024-07-29 23:46:02 -0300

    Docstring

--- app.py
+++ app.py
@@ -11,31 +11,49 @@ restaurantes = [{'nome':'Pizza Mario', 'categoria':'Italiana', 'ativo':False},
 
 #Definições/Funções
 def voltar_ao_menu_principal():
+    '''função para voltar ao menu principal da aplicação'''
+    #acima temos uma dockstring string de documentação das funções, logoabaixo da assinatura da função
+
     input('\nDigite uma tecla para voltar ao menu.')
     main()
 
 def exibir_subtitulo(texto):
+    '''Exibe subtítulo das opções do programa'''
+
     os.system('cls')
     linha = '*' * (len(texto) + 4)
     print(f'{linha}\n{texto}\n{linha}\n')
 
 def exibir_nome_do_programa():
+    '''Exibe título do programa'''
+
     print('\nSabor Express')
     
 def exibir_opcoes():
+    '''Exibe as opções do menu inicial do programa'''
     print('1 - Cadastrar restaurante')
     print('2 - Listar restanrantes')
     print('3 - Alterar status do restaurante')
     print('4 - Sair')
 
 def finalizar_app():
+    '''Exibe título de encerramento do programa e finaliza o app pois não há instruções após chamar esta função'''
     exibir_subtitulo('Programa encerrado.')
 
 def opcao_invalida():
+    '''Quando o usuário digita alguma função errada'''
     print('Opção inválida!')
     voltar_ao_menu_principal()
 
 def cadastrar_restaurante():
+    '''Onde se cadastra um restaurante
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Output
+    - Adiciona um novo restaurante a lista de restaurantes
+    '''
     exibir_subtitulo('Cadastrar restaurante')
     nome_do_restaurante = input('Digite o nome do restaurante: ')
     categoria_do_restaurante = input('Digite a categoria do restaurante: ')
@@ -45,6 +63,7 @@ def cadastrar_restaurante():
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
+    '''Lista os restaurantes'''
     exibir_subtitulo('Listar restaurantes')    
     print(f'{'Nome'.ljust(23)} | {'Categoria'.ljust(20)} | {'Status'}')
     for restaurante in restaurantes:
@@ -53,6 +72,14 @@ def listar_restaurantes():
     voltar_ao_menu_principal()
 
 def alterar_status_restaurante():
+    '''Altera o status de um restaurante
+    
+    Input
+    - Nome do restaurante
+
+    Output
+    - Inverte o valor booleano do status do restaurante    
+    '''
     exibir_subtitulo('Alterar status de restaurante')
 
     nome_restaurante = input('Digite o nome do restaurante que deseja alterar o status: ')
@@ -70,6 +97,7 @@ def alterar_status_restaurante():
     voltar_ao_menu_principal()
 
 def escolher_opcao():
+    '''Navega nas opções do programa'''
     try:
         opcao_escolhida = int(input('\nEscolha uma opção: '))
         match opcao_escolhida:
@@ -87,6 +115,7 @@ def escolher_opcao():
         opcao_invalida()
 
 def main():
+    '''Função principal do programa que o inicia'''
     os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
